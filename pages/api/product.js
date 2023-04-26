65293a25cbeff382be9f9d98a5217ff9c95e527a
Dev Eduardo Cauan <eduardoedif10@gmail.com>
2023-04-25 23:09:38 -0300

    nwes
    dssds

--- pages/api/product.js
+++ pages/api/product.js
@@ -19,18 +19,25 @@ function runMiddleware(req, res, fn) {
 
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
-  const result = await excuteQuery({
+  const result = await executeQuery({
     query:
       "SELECT * FROM product WHERE 1 == ?",
     values: [1],
   });
+
   console.log(result);
+
+
+  if (result.error) {
+    res.status(500).json({ error: "Error executing the query" });
+    return;
+  }
+
+
+
   return res.status(200).json(result);
 
 }
 
-if (results.error) {
-  res.status(500).json({ error: "Error executing the query" });
-  return;
-}
+
 
