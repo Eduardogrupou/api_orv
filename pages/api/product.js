ad4292fb90a749e2033af5d4973f73bb69d0eaf8
Dev Eduardo Cauan <eduardoedif10@gmail.com>
2023-04-25 22:53:20 -0300

    confirm

--- pages/api/product.js
+++ pages/api/product.js
@@ -21,26 +21,14 @@ export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
   // Consulta para selecionar todos os produtos da tabela "perfumes"
-  const query = "SELECT id, name, image, dcp, gender, brand_id, destaque FROM product";
-  const results = await executeQuery({ query });
+  const query = "SELECT * FROM product";
+  const products = await executeQuery({ query });
 
   if (results.error) {
     res.status(500).json({ error: "Error executing the query" });
     return;
   }
 
-  // Mapeia os resultados da consulta para um objeto com os atributos desejados
-  const products = results.map((row) => ({
-    id: row.id,
-    name: row.name,
-    image: row.image,
-    dcp: row.dcp,
-
-
-
-
-
-  }));
 
   res.status(200).json({ data: products });
 }
