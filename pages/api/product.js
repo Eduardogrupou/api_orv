1a96954f7814b96482ce1cbc695af7b05d0933c8
Eduardouniia <130407161+Eduardouniia@users.noreply.github.com>
2023-04-30 01:05:56 -0300

    up
    wsdf

--- pages/api/product.js
+++ pages/api/product.js
@@ -19,7 +19,52 @@ function runMiddleware(req, res, fn) {
 
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
+  cont {filter, value, limit} = req.params
 
+  if (filter){
+    const query = `SELECT * FROM product WHERE ${filter} = ${value} `;
+  const results = await executeQuery({ query });
+    
+     const products = results.map((row) => ({
+    id: row.id,
+    name: row.name,
+    image: row.image,
+    dcp: row.dcp,
+    gender: row.gender,
+    brand_id: row.brand_id,
+    destaque: row.destaque,
+       
+       )}
+      res.send.json(products)
+  }
+                                  
+                                  
+  if (limit)  {
+     
+       // Consulta para selecionar todos os produtos da tabela "perfumes"
+  const query = "SELECT * FROM product LIMIT" + limit;
+  const results = await executeQuery({ query });
+
+  if (results.error) {
+    res.status(500).json({ error: "Error executing the query" });
+    return;
+  }
+  
+  // Mapeia os resultados da consulta para um objeto com os atributos desejados
+  const products = results.map((row) => ({
+    id: row.id,
+    name: row.name,
+    image: row.image,
+    dcp: row.dcp,
+    gender: row.gender,
+    brand_id: row.brand_id,
+    destaque: row.destaque,
+
+)}
+
+  res.status(200).json({ data: products });
+     
+     }
   // Consulta para selecionar todos os produtos da tabela "perfumes"
   const query = "SELECT * FROM product";
   const results = await executeQuery({ query });
@@ -28,7 +73,7 @@ export default async function handler(req, res) {
     res.status(500).json({ error: "Error executing the query" });
     return;
   }
-
+  
   // Mapeia os resultados da consulta para um objeto com os atributos desejados
   const products = results.map((row) => ({
     id: row.id,
