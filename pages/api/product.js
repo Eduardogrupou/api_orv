702ba8e6a8831e99d9819618a14bf4d7bb5b7505
Eduardouniia <130407161+Eduardouniia@users.noreply.github.com>
2023-04-30 01:58:40 -0300

    asd
    dxcsdzxc

--- pages/api/product.js
+++ pages/api/product.js
@@ -1,3 +1,5 @@
+
+
 import Cors from "cors";
 import executeQuery from "../../lib/db";
 
@@ -20,7 +22,33 @@ function runMiddleware(req, res, fn) {
 export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
-  // Consulta para selecionar todos os produtos da tabela "perfumes"
+  const {filter} = req.query;
+  const {values} = req.params;
+  if {filter} {
+    const query = `SELECT * FROM product WHERE ${filter} = ${values}`};
+    const results = await executeQuery({ query });
+
+    if (results.error) {
+      res.status(500).json({ error: "Error executing the query" });
+      return;
+    }
+    if (!results|| !value )  
+      res.status(300).json({ error: "Error executing the query" });
+      return;
+    }
+
+    const products = results.map((row) => ({
+      id: row.id,
+      name: row.name,
+      image: row.image,
+      dcp: row.dcp,
+      gender: row.gender,
+      brand_id: row.brand_id,
+      destaque: row.destaque,
+
+    }));
+    res.status(200).json({ data: products });
+  }
   const query = "SELECT * FROM product";
   const results = await executeQuery({ query });
 
@@ -29,7 +57,7 @@ export default async function handler(req, res) {
     return;
   }
 
-  // Mapeia os resultados da consulta para um objeto com os atributos desejados
+
   const products = results.map((row) => ({
     id: row.id,
     name: row.name,
@@ -49,3 +77,4 @@ export default async function handler(req, res) {
 
   res.status(200).json({ data: products });
 }
+
