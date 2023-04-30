990c30db25d8beb2eddb28514460429c8b34ddeb
Eduardouniia <130407161+Eduardouniia@users.noreply.github.com>
2023-04-30 01:48:03 -0300

    ededas
    adzfsdx

--- pages/api/product.js
+++ pages/api/product.js
@@ -5,67 +5,47 @@ const cors = Cors({
   methods: ["POST", "GET", "HEAD"],
 });
 
-async function handler(req, res) {
-  await runMiddleware(req, res, cors);
-  const { filter, value, limit } = req.query;
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
 
-if (filter) {
-const query = SELECT * FROM product WHERE ${filter} = ?;
-const results = await executeQuery({ query, params: [value] });
-    const products = results.map((row) => ({
-      id: row.id,
-      name: row.name,
-      image: row.image,
-      dcp: row.dcp,
-      gender: row.gender,
-      brand_id: row.brand_id,
-      destaque: row.destaque,
-    }));
-    res.status(200).json({ data: products });
-  } else if (limit) {
-const query = SELECT * FROM product LIMIT ${limit};
-const results = await executeQuery({ query });
-
-    if (results.error) {
-      res.status(500).json({ error: "Error executing the query" });
-      return;
-    }
-
-    // Mapeia os resultados da consulta para um objeto com os atributos desejados
-    const products = results.map((row) => ({
-      id: row.id,
-      name: row.name,
-      image: row.image,
-      dcp: row.dcp,
-      gender: row.gender,
-      brand_id: row.brand_id,
-      destaque: row.destaque,
-    }));
-
-    res.status(200).json({ data: products });
-  } else {
-    // Consulta para selecionar todos os produtos da tabela "perfumes"
-    const query = "SELECT * FROM product";
-    const results = await executeQuery({ query });
-
-    if (results.error) {
-      res.status(500).json({ error: "Error executing the query" });
-      return;
-    }
-
-    // Mapeia os resultados da consulta para um objeto com os atributos desejados
-    const products = results.map((row) => ({
-      id: row.id,
-      name: row.name,
-      image: row.image,
-      dcp: row.dcp,
-      gender: row.gender,
-      brand_id: row.brand_id,
-      destaque: row.destaque,
-    }));
-
-    res.status(200).json({ data: products });
-  }
+      return resolve(result);
+    });
+  });
 }
 
+export default async function handler(req, res) {
+  await runMiddleware(req, res, cors);
 
+  // Consulta para selecionar todos os produtos da tabela "perfumes"
+  const query = "SELECT * FROM product";
+  const results = await executeQuery({ query });
+
+  if (results.error) {
+    res.status(500).json({ error: "Error executing the query" });
+    return;
+  }
+
+  // Mapeia os resultados da consulta para um objeto com os atributos desejados
+  const products = results.map((row) => ({
+    id: row.id,
+    name: row.name,
+    image: row.image,
+    dcp: row.dcp,
+    gender: row.gender,
+    brand_id: row.brand_id,
+    destaque: row.destaque,
+
+
+
+
+
+
+
+  }));
+
+  res.status(200).json({ data: products });
+}
